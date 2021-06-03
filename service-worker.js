8bb4d73076bff62a1f13c211ede9853d602a74a0
Chris Haack <chris.haack@me.com>
2021-06-03 12:20:28 -0400

    Release at Thu Jun  3 12:20:28 EDT 2021

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a3be07c15a14b03c19befd55a4ca2c9.js"
+  "/react_portfolio/precache-manifest.854f2de9b54125e4cf5153c8431fed2c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react_portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
