66d8818538d59dc0724028b3a5e842142fc17489
Chris Haack <chris.haack@me.com>
2021-06-03 11:44:08 -0400

    Initial build subtree commit

---
+++ precache-manifest.854f2de9b54125e4cf5153c8431fed2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76ddc266277e9b52c9e7",
+    "url": "/react_portfolio/static/css/main.59e9fc07.chunk.css"
+  },
+  {
+    "revision": "76ddc266277e9b52c9e7",
+    "url": "/react_portfolio/static/js/main.76ddc266.chunk.js"
+  },
+  {
+    "revision": "8a2b2096fd90bcd7ad6e",
+    "url": "/react_portfolio/static/js/runtime~main.8a2b2096.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/react_portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "e7764a3a39463c598c24908f68b3c8e0",
+    "url": "/react_portfolio/index.html"
+  }
+];
